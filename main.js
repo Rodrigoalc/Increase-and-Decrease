612f3fa22cbdd7cf955fcf720caaddec775a11af
Rodrigo Lima <roddesign.contato@gmail.com>
2023-07-19 22:00:04 -0300

    :tada:

---
+++ main.js
@@ -0,0 +1,15 @@
+function IncreaseCounter() {
+  const counter = document.getElementById("counter")
+  let value = parseInt(counter.textContent)
+  value++
+  counter.textContent = value.toString().padStart(2, "0")
+}
+
+function DecreaseCounter() {
+  const counter = document.getElementById("counter")
+  let value = parseInt(counter.textContent)
+  if (value > 1) {
+    value--
+    counter.textContent = value.toString().padStart(2, "0")
+  }
+}
